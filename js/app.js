94ac08d2acf6834d855d1fee1f8e1e83cf973220
simona <simonaulloa@gmail.com>
2017-06-01 02:27:15 -0400

    casi casi

--- js/app.js
+++ js/app.js
@@ -3,31 +3,47 @@
 	lo que hizo la blanca - veamos si kxamos U-U
 */
 
- (function main(){
-	var boxes = Array.from(document.getElementsByClassName("box-services1"));
-	
-	var modal = document.getElementById("box-services1-modal");
-	var bodyModal;
-	
+(function main(){
+	var boxes = Array.from(document.getElementsByClassName("box-work"));
+	var modal = document.getElementById("box-work-modal");
+	var bodyModal, close, img;
 	boxes.forEach(function(box){
 		box.addEventListener("click", function(){
-			modal.classList.remove("hide")
-		bodyModal = document.createElement("div");
+			modal.innerHTML = "";
+			bodyModal = document.createElement("div");
+			bodyModal.classList.add("modal-body");
+			bodyModal.innerHTML = box.innerHTML;			
+			modal.appendChild(bodyModal);
+			modal.classList.remove("hide");
+			close = document.createElement("div");
+			close.classList.add("close");
+			img = document.createElement("img");
+			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+			close.appendChild(img);
+			modal.appendChild(close);
+			close.addEventListener("click",function(){
+				modal.classList.add("hide");
+			});
+		});		
+	});
+})();
 
-		bodyModal.classList.add("modal-body");
-		bodyModal.innerHTML = box.innerHTML;
-		modal.appendChild(bodyModal);
-		 
-		});
-	});	
+	
 
-	var close = document.querySelector("box-services1-modal .close"); 		
-		close.addEventListener("click" function(){
-			modal.classList.add("hide");
-		})
-})();			
+/*
+funcion autoinvocada
 
-			
+(function miAlert(){
+	....
+})()
+
+Es equivalente a:
+
+function miAlert(){
+	...
+};
+
+miAlert(); */		
 
 /*
 funcion autoinvocada
